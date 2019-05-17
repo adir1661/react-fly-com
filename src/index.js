57ed8dbdd2eca5eb2dc5207b3887136605c0229c
danavcohen <33395874+danavcohen@users.noreply.github.com>
2019-05-17 12:27:40 +0300

    push

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Search from './our-components/ourSearch';
+import Search from './components/ourSearch';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router,Route,browserHistory ,Link,Switch } from 'react-router-dom';
 ReactDOM.render(
